cb87f34d3edff1ec76f038264c3d07f79879a077
Farrukh <sh.farrukh99@mail.ru>
2021-04-06 11:24:11 +0300

    Task 12.1, created test.go

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -4,28 +4,35 @@ import (
 	"github.com/shFarrukh/bank/v2/pkg/types"
 )
 
-
-func Avg(payments []types.Payment) types.Money{
+func Avg(payments []types.Payment) types.Money {
 	s := 0
 	couter := 0
 	for _, i := range payments {
-		
-		if i.Status != types.StatusFail{
-			s+= int(i.Amount)
-		couter ++
+
+		if i.Status != types.StatusFail {
+			s += int(i.Amount)
+			couter++
 		}
 	}
-	return types.Money(s/couter)
+	return types.Money(s / couter)
 
 }
-func TotalInCategory (Payments []types.Payment, category types.Category) types.Money{
-	s:=0
+func TotalInCategory(Payments []types.Payment, category types.Category) types.Money {
+	s := 0
 
-	for _, i := range Payments{
-		if i.Category == category && i.Status != types.StatusFail{
+	for _, i := range Payments {
+		if i.Category == category && i.Status != types.StatusFail {
 			s += int(i.Amount)
-			
+
 		}
 	}
 	return types.Money(s)
+}
+
+func CategoriesAvg(payments []types.Payment) map[types.Category]types.Money {
+	ans := map[types.Category]types.Money{}
+	for _, i := range payments {
+		ans[i.Category] = TotalInCategory(payments, i.Category)
+	}
+	return ans
 }
---
+++ pkg/stats/stats_test.go
@@ -0,0 +1,37 @@
+package stats
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/shFarrukh/bank/v2/pkg/types"
+)
+
+func TestCategoriesAvg(t *testing.T) {
+	payments := []types.Payment{
+		{
+			Category: "food",
+			Amount:   1000,
+		},
+		{
+			Category: "food",
+			Amount:   1000,
+		},
+		{
+			Category: "auto",
+			Amount:   5000,
+		},
+		{
+			Category: "auto",
+			Amount:   5000,
+		},
+	}
+	expected := map[types.Category]types.Money{
+		"auto":     5000,
+		"android": 1000,
+	}
+	result := CategoriesAvg(payments)
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
+	}
+}
